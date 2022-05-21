0f9de70c9931dbcec824614282ff37b8bac9e764
Sayujya Malkan <sayujyamalkan@gamil.com>
2022-05-21 23:50:53 +0530

    Added Home

---
+++ client/src/components/HomeNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeNav = () => {
+  return (
+    <div id='home-nav-container' style={{height:"10vh", padding:"3rem 0rem 0rem 6rem"}} >
+        <img src='./images/logo.png' alt='logo' height={30} />
+    </div>
+  )
+}
+
+export default HomeNav
---
+++ client/src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import HomeNav from '../components/HomeNav'
+import "../styles/Home.css"
+
+const Home = () => {
+  return (
+    <div>
+        <HomeNav />
+        <div id='home-container'>
+            <h1 id='home-head'>Invoice Generator</h1>
+            <div id='creator-container'>
+                <input id='invoice-create' type='text' placeholder='Enter Name' />
+                <input id='invoice-create-btn' type='button' value='Generate Invoice' />
+            </div>
+        </div>
+        <div id='saved-invoices'>
+            <div>
+                <h2 style={{fontFamily:"Prompt"}}>Saved Invoices</h2>
+                <div>
+                    {/* Here there will be a map function which will fetch all the invioices */}
+                    <div className='invoice-card'>
+                        <p className='invoice-card-head'>invoice1</p>
+                        <p>15th May 2022</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src='./images/invoice.png' alt='invoice' id='inv-img' />
+    </div>
+  )
+}
+
+export default Home
